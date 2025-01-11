c10d4394b35c1ad4fb1374f0c152fe2a0a9193c6
Oleh Hanhal <hanhaloleg59@gmail.com>
2025-01-11 21:45:36 +0200

    add views for books app

--- library/order/views.py
+++ library/order/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from .models import Order
+from .models import Book
 from authentication.models import CustomUser
 from django.utils import timezone
 
